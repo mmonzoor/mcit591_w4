bfd943986cbcb0c5070efe38703bb6ada4346e26
mmonzoor <mmonzoor.wv@gmail.com>
2019-06-20 17:39:16 -0400

    made more changes

--- Sample.java
+++ Sample.java
@@ -1,11 +1,9 @@
-cclass Sample {
+class Sample {
     public static void main(String[] args) {
-        // feel free to edit the line below and change the loop maximum to a different
-        // boundary.
-        // Update! I moved the print statement outside of the loop to see the sum of the for loop you created
     	double sum = 0;
         for (int i = 0; i < 5; i++) {
         	sum += i; 
+            System.out.print(i+1);
         }
         System.out.println(sum);
     }
